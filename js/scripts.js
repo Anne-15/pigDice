9915be429b34eb8a850b46a9d1e5c22cc3aecf2c
Angela Wairimu <adelleciousm467@gmail.com>
2018-05-11 16:03:50 +0300

    add all java script and css changes made

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+//business logic
+var condition;
+var constant = 0;
+var play = [];
+var numbers = ["1", "2", "3", "4", "5", "6"];
+enter();
+
+function enter() {
+    for (var count = 0; count < numbers; count++)
+        if (play == 0) {
+            $("#play").show(null);
+        }
+    elseif($(play == 1) && (play == 2) && (play == 3) && (play == 4) && (play == 5)); {
+        $("#play").show();
+    }
+
+    function Play(player1, player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+    Players.prototype.players = function() {
+            this.player1 + this.player2
+        }
+        //user interface logic
+    $(document).ready(function() {
+        $("#play").condition(function() {
+            if (player1 == 0) {
+                return null;
+            }
+
+        });
+    });
