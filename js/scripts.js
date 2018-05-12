f0bb11bb521fc9521e3a2c99918005b58bf87e7d
Angela Wairimu <adelleciousm467@gmail.com>
2018-05-12 23:00:31 +0300

    add js text and changes made to the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,37 @@
 //business logic
 var condition;
-var constant = 0;
-var play = [];
-var numbers = ["1", "2", "3", "4", "5", "6"];
-enter();
+var element1 = function(currentScore) {
+    var dice = Math.floor((Math.random() * 6) + 1);
+    if (dice < 2) {
+        var currentScore = 0;
+        return currentScore;
+    } else if (dice >= 2) {
+        currentScore += dice
+        var i = 0;
+        return currentScore;
+    };
+};
 
-function enter() {
-    for (var count = 0; count < numbers; count++)
-        if (play == 0) {
-            $("#play").show(null);
-        }
-    elseif($(play == 1) && (play == 2) && (play == 3) && (play == 4) && (play == 5)); {
-        $("#play").show();
-    }
 
-    function Play(player1, player2) {
-        this.player1 = player1;
-        this.player2 = player2;
-    }
-    Players.prototype.players = function() {
-        this.player1 + this.player2
-    }
+var Player = function(result1, result2, play1, play2, ) {
+    var result1 = 0;
+    var result2 = 0
+    var play1 = 0;
+    var play2 = 0;
+    var play = [];
 }
+
+function Play(numbers) {
+    this.numbers = numbers;
+};
+
+$("#p1").text();
+$("#p2").text();
+
 //user interface logic
+//roll dice
+$(document).click(function() {
+    $("#show").click(function() {
+
+    });
+});
