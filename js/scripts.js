574fa36bfddb60de2a3ef0613c4506cf500347b8
Angela Wairimu <adelleciousm467@gmail.com>
2018-05-12 23:33:37 +0300

    add the js text changes made to the repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,51 @@
 //business logic
 var condition;
-var element1 = function(currentScore) {
-    var dice = Math.floor((Math.random() * 6) + 1);
-    if (dice < 2) {
+var player = function(currentScore) {
+    var dice = Math.floor(Math.random());
+    if (dice < 1) {
         var currentScore = 0;
-        return currentScore;
+        return null;
     } else if (dice >= 2) {
-        currentScore += dice
+        currentScore += numbers
         var i = 0;
         return currentScore;
     };
 };
 
 
-var Player = function(result1, result2, play1, play2, ) {
+var Game = function(result1, result2, play1, play2, ) {
     var result1 = 0;
     var result2 = 0
     var play1 = 0;
     var play2 = 0;
-    var play = [];
 }
-
-function Play(numbers) {
-    this.numbers = numbers;
-};
-
 $("#p1").text();
 $("#p2").text();
+$("#dice").show();
+
 
 //user interface logic
 //roll dice
-$(document).click(function() {
-    $("#show").click(function() {
 
-    });
-});
+$("button#roll").click(function() {
+            if (index === 0) {
+                p1 = player(p1);
+                if (p1 > 0) {
+                    $("#dice").text(p1);
+                } else {
+                    $("#dice").text("1");
+                }
+                dice = p1
+                $("#point").text(result1)
+                if (p1 === 0) {
+                    index = 1;
+                    dice = 0;
+                    $("#player1").hide();
+                    $("#player2").show();
+                } else if ((p1 + total1) >= 100) {
+                    total1 += result1;
+                    $("#p1").text(total1);
+                    alert("Player 1 Win!");
+                    index = 2;
+                }
+            }
