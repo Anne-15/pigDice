e0355efb235bff703dba3e5d99e631f2d8cdf7bd
Angela Wairimu <adelleciousm467@gmail.com>
2018-05-13 07:44:36 +0300

    add js to the repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //business logic
 var condition;
 var player = function(currentScore) {
-    var dice = Math.floor(Math.random());
+    var dice = ("1", "2", "3", "4", "5", "6");
     if (dice < 1) {
         var currentScore = 0;
         return null;
@@ -49,7 +49,28 @@ $("button#roll").click(function() {
                 } else if ((p1 + total1) = 100) {
                     total1 += result1;
                     $("#p1").text(total1);
-                    alert("Player 1 Win!");
+                    alert("Player 1 Wins!");
                     index = 2;
+                };
+            };
+            if (index === 1) {
+                p2 = player(p2);
+                if (p2 > 0) {
+                    $("#dice").text(p2);
+                } else {
+                    ("#dice").text("1");
                 }
-            }
+                dice = p2
+                $("#point").text(result2)
+                if (p2 === 0) {
+                    index = 1;
+                    dice = 0;
+                    $("#player1").show();
+                } else if ((p2 + total2) = 100) {
+                    total2 += result2;
+                    $("p2").text(total2);
+                    alert("Player 2 Wins!")
+                    index = 2;
+                };
+            };
+        };
