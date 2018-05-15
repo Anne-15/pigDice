857dd346ac8bf46b2cc224f7958f9e5f37ed9ae0
Angela Wairimu <adelleciousm467@gmail.com>
2018-05-15 07:51:48 +0300

    add js and html

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,10 @@
 condition;
 var counter = math.floor((math.random) + 1);
 var dice = (1, 2, 3, 4, 5, 6);
-var dice = 0;
 if (index === 0) {
     counter = null;
     return null;
-}
-elseif(index > 2) {
+} else if (index > 2) {
     counter += dice;
     var i = 0
     return counter;
@@ -29,6 +27,83 @@ Player.prototype.counter = function() {
 //play button 
 $(document).click(function() {
     $("button#play").click(function() {
-        if (index === 0)
+        if (index === 0) {
+            result1 = dice(result1);
+            if (result1 > 1) {
+                $("#dice").text(result1 - dice);
+            } else {
+                $("#dice").text("2");
+            }
+            counter = result1
+            $("#score").text(result1)
+            if (result1 === 0) {
+                index = 1;
+                dice = 0;
+                $("#player1").hide();
+                $("#player2").show();
+            } else if ((result1 + total1) = 100) {
+                total1 += result1;
+                $("#result1").text(total1);
+                alert("Player 1 Win!");
+                index = 2;
+            }
+        } else if (index === 1) {
+            result2 = dice(result2);
+            if (result2 > 0) {
+                $("#dice").text(result2 - dice);
+            } else {
+                $("#dice").text("1");
+            }
+            counter = result2
+            $("#score").text(result2)
+            if (result2 === 0) {
+                index = 0;
+                dice = 0;
+                $("#player2").hide();
+                $("#player1").show();
+            } else if ((result2 + total2) = 100) {
+                total2 += result2;
+                $("#result2").text(total2);
+                alert("Player 2 Win!");
+                index = 2;
+            }
+        }
     });
+});
+
+$("button#pass").click(function() {
+    if (index === 1) {
+        index = 0;
+        dice = 0;
+        total2 += result2;
+        $("#result2").text(total2);
+        $("#score").text(0)
+        result2 = 0;
+        $("#player2").hide();
+        $("#player1").show();
+    } else if (index === 0) {
+        index = 1;
+        dice = 0;
+        total1 += result1;
+        $("#result1").text(total1);
+        $("#score").text(0)
+        result1 = 0;
+        $("#player1").hide();
+        $("#player2").show();
+    }
+});
+
+$("button#restart").click(function() {
+total1 = 0;
+total2 = 0;
+index = 0;
+result1 = 0;
+result2 = 0;
+$("#player2").hide();
+$("#player1").show();
+$("#score").text(0)
+$("#result1").text(0);
+$("#result2").text(0);
+})
+
 });
